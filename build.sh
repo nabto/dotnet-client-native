88434fb9712c417a1fb23961278187fff2e767c4
Michael Madsen <mkm@nabto.com>
2017-07-20 09:38:16 +0200

    added version number whcih is configurable at build run

--- build.sh
+++ build.sh
@@ -4,5 +4,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/NabtoClientNative
 
+VERSION_ARG=""
+
+if [ "$1" != "" ]; then
+    VERSION_ARG="/p:Version=$1"
+fi
+
+ARTIFACTS_DIR=$DIR/artifacts
+
+mkdir -p $ARTIFACTS_DIR
+
 dotnet restore
-dotnet pack -c Release
+dotnet pack $VERSION_ARG -o $ARTIFACTS_DIR -c Release
--- docker-build.sh
+++ docker-build.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 chmod +x $DIR/build.sh
 
-docker run -i -t -v $DIR:/source microsoft/dotnet /source/build.sh
+docker run -i -t -v $DIR:/source microsoft/dotnet /source/build.sh $@
